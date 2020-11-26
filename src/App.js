e3ab8d133eb995aaa59643a64113865be15c0e53
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-11-26 18:39:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Page from './components/page';
+import Product from './components/Product';
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Switch>
-          <Route path='/' component={Page} />
+
+          <Route path='/product/:id' component={Product} />
+          <Route exact path='/' component={Page} />
         </Switch>
       </BrowserRouter>
     </React.Fragment>
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,36 +2,44 @@ import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import Loading from './Loading'
 
-class Page extends Component {
+class Product extends Component {
 
     constructor(props){
         super(props)
         this.state = {
-            products: []
+            products: {}
         }
     }
 
     componentDidMount(){
-        fetch('https://fakestoreapi.com/products')
+        //console.log('props', this.props)
+        fetch(`https://fakestoreapi.com/products/${this.props.match.params.id}`)
         .then(resp => resp.json())
         .then(res => {
             //console.log(res)
             this.setState({products: res})
+            //console.log(this.state.products)
         })
         .catch(err => console.log(err))
     }
 
     render(){
-       const products = this.state.products && this.state.products.map((product)=>{
-            return(
-                    <Link to={`product/${product.id}`} key={product.id}>
-                    <div className="col s12 m6 l3" >
-                    <div className="card hoverable medium">
+        let product = this.state.products ? this.state.products : {};
+            
+        return(
+            <React.Fragment>
+                <div className='container'>
+                <div className="row" >
+                {console.log('products length:' , product.image)}
+                    {this.state.products  ? 
+
+                    <div className="col s12" >
+                    <div className="card hoverable ">
                         <div className="card-image">
                         <img alt='product' src={product.image} height='200rem' />
                         <div>
                         <span className="card-title grey-text text-darken-4 z3"><h6>{product.title}</h6></span>
-                        <Link to ={`product/${product.id}`} className="btn-floating right waves-effect waves-light red">
+                        <Link to ='/add' className="btn-floating right waves-effect waves-light red">
                             <i className="material-icons">add</i>
                         </Link>
                         <span><h5>${product.price}</h5></span>
@@ -43,15 +51,7 @@ class Page extends Component {
                         </div>
                     </div>
                     </div>
-                    </Link>
-                
-            )
-        })
-        return(
-            <React.Fragment>
-                <div className='container'>
-                <div className="row" >
-                    {products.length > 1 ? products : <Loading />}
+                : <Loading />}
                 </div>
                 </div>
             </React.Fragment>
@@ -60,4 +60,4 @@ class Page extends Component {
 }
 
 
-export default Page;
+export default Product;
